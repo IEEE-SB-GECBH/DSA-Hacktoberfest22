5c6e891ee8aa59b9d75aa6fe5aa831d09d5baadb
mariatomy9 <mariatomy555@gmail.com>
2022-10-24 08:58:34 +0530

    New Codes2.0

---
+++ python/Hashing.py
@@ -0,0 +1,15 @@
+#Hashing
+def mod(number, cellNumber):
+    return number % cellNumber
+
+
+# print(mod(400, 24))
+
+
+def modASCII(string, cellNumber):
+    total = 0
+    for i in string:
+        total += ord(i)
+    return total % cellNumber
+
+print(modASCII("ABC", 24))
